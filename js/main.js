fd3f22df2dc51d5f5d9c91926f6f348cff787516
Mila Ziabchenko <mi.podgurska@gmail.com>
2021-11-02 12:01:37 +0200

    modified:   css/style.css 	modified:   index.html 	modified:   js/main.js 	modified:   scss/_main.scss 	new file:   scss/_mediaqueries.scss 	modified:   scss/_variables.scss 	modified:   scss/style.scss

--- js/main.js
+++ js/main.js
@@ -51,33 +51,36 @@ const getWinner = (p, c) => {
 };
 
 const showWinner = (winner, computerChoice) => {
-  if (winner === 'player') {
-    scoreBoard.player++;
-    result.innerHTML = `
+  if (scoreBoard.player < 3 && scoreBoard.computer < 3) {
+    if (winner === 'player') {
+      scoreBoard.player++;
+      result.innerHTML = `
         <h2 class="text-win">You Win</h2>
         <img src="img/${computerChoice}.svg"/>
         <h3>Computer Chose <strong>${
           computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
         }</strong></h3>
         `;
-  } else if (winner === 'computer') {
-    scoreBoard.computer++;
-    result.innerHTML = `
+    } else if (winner === 'computer') {
+      scoreBoard.computer++;
+      result.innerHTML = `
         <h2 class="text-lose">You Lose</h2>
         <img src="img/${computerChoice}.svg"/>
         <h3>Computer Chose <strong>${
           computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
         }</strong></h3>
         `;
-  } else {
-    result.innerHTML = `
+    } else {
+      result.innerHTML = `
         <h2>It's A Draw</h2>
         <img src="img/${computerChoice}.svg"/>
         <h3>Computer Chose <strong>${
           computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
         }</strong></h3>
         `;
+    }
   }
+
   score.innerHTML = `
         <p>Computer: ${scoreBoard.computer}</p>
         <p>You: ${scoreBoard.player}</p>
